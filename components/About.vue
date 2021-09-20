00b2d557ab4a5f6e95651f766510b721f85c5168
Pau Abella <pauabellamolina1@gmail.com>
2021-09-20 18:51:17 +0200

    Heading and about done and getting texts from the api. Colors and style tweaks still todo

---
+++ components/About.vue
@@ -0,0 +1,33 @@
+<template>
+  <span class="flex flex-col justify-center">
+    <div class="playfair mc-title">
+      <p>About</p>
+    </div>
+    <div class="mc-about-content" v-html="abouttexts">
+      <!-- <p>I am a 19 year old guy based in Reus, a city near the every time more techie Barcelona.</p>
+      <p>I love climbing, surfing and coding.</p>
+      <p>I also enjoy designing and sketching webpages, logos, posters, cd covers... from time to time but I consider myself much more of a developer than a designer.</p> -->
+      {{ abouttexts }}
+    </div>
+  </span>
+</template>
+
+<script>
+export default {
+  props: ['abouttexts']
+}
+</script>
+
+<style>
+.mc-title {
+  padding-left: 1vw;
+  font-size: 5vw;
+}
+
+.mc-about-content p {
+  text-align: justify;
+  font-size: 1.5vw;
+  line-height: 1.9vw;
+  padding: .5vw 0 .5vw 0;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,95 @@
 <template>
-  <Tutorial />
+  <span class="parent flex flex-col">
+    <span class="flex flex-col">
+      <div class="top-heading playfair">
+        <p>Hi. My name is</p>
+      </div>
+      <div class="main-heading">
+        <p>PAU ABELLA</p>
+      </div>
+      <div class="sub-heading">
+        <!-- <p>I am a Junior Software Developer highly passionate about engineering and design.</p>
+        <p>Making cool things fast and pretty is the goal.</p> -->
+        <p v-if="$fetchState.pending">...</p>
+        <p v-else-if="$fetchState.error">:(</p>
+        <div v-else v-html="maintexts[0].subheading"> {{ maintexts[0].subheading }}</div>
+      </div>
+    </span>
+
+    <div class="main-content">
+      <About :abouttexts="maintexts[0].about" />
+    </div>
+  </span>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      maintexts: []
+    }
+  },
+  async fetch () {
+    this.maintexts = await fetch(
+      'http://localhost:3030/main-texts'
+    ).then(res => res.json())
+  },
+  head () {
+    return {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Playfair+Display:wght@900&display=swap'
+        }
+      ]
+    }
+  }
+}
 </script>
+
+<style>
+@font-face {
+  font-family: 'Helvetica';
+  font-display: swap;
+  src: url('~assets/fonts/Helvetica-Bold.ttf') format('truetype');
+}
+@font-face {
+  font-family: 'Gobold';
+  font-display: swap;
+  src: url('~assets/fonts/Gobold High Bold.otf') format('truetype');
+}
+
+.body {
+  background-color: #000202;
+}
+
+p {
+  color: #fff;
+  font-family: 'Helvetica', sans-serif;
+}
+
+.playfair, .playfair p {
+  font-family: 'Playfair Display', serif !important;
+}
+
+.top-heading p {
+  font-size: 7.6vw;
+  line-height: 130%;
+  padding-left: 3.2vw;
+}
+.main-heading p {
+  font-family: 'Gobold', sans-serif;
+  font-size: 36vw;
+  text-align: center;
+  line-height: 105%;
+}
+.sub-heading p {
+  font-size: 1.82vw;
+  padding-left: 3.2vw;
+}
+
+.main-content {
+  margin-top: 8vw;
+  padding: 0 10vw 0 10vw;
+}
+</style>
